bd96b99b61b768edef09faf50ecb892f3d354438
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-08-17 20:38:25 -0300

    fix - adjust component"

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import Avatar_Elielson from "../assets/Avatar_Elielson.png";
 export default function Profile() {
   return (
     <main className="justify-center flex-wrap content-center flex flex-col items-center">
-      <img src={Avatar_Elielson} alt="" className="w-24 h-24 rounded-full " />
+      <img src={Avatar_Elielson} alt="Elielson Profile" className="w-24 h-24 rounded-full " />
       <h1 className="text-2xl font-bold">Elielson Nascimento</h1>
       <a className="mt-1">@elielsondev</a>
     </main>
--- src/components/PropChildren.jsx
+++ src/components/PropChildren.jsx
@@ -1,7 +1,7 @@
 export default function PropChildren(props) {
   return (
     <button className="bg-indigo-700 font-bold w-56 px-4 py-2 rounded hover:bg-slate-100 hover:text-green-600">
-      <a href={props.href} target="_blank" rel="noopener noreferrer">
+      <a href={props.href} target="_blank">
         { props.children }
       </a>
     </button>
