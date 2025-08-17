af3711a6c3ea883b6ec1d876fcf942118ab2b5aa
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-08-17 19:32:25 -0300

    feat - add image profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,11 @@
+import Avatar_Elielson from "../assets/Avatar_Elielson.png";
+
 export default function Profile() {
   return (
-    <main className="justify-center flex-wrap">
+    <main className="justify-center flex-wrap content-center flex flex-col items-center">
+      <img src={Avatar_Elielson} alt="" className="w-24 h-24 rounded-full" />
       <h1 className="text-3xl font-bold">Elielson Nascimento</h1>
-        <a className="mt-4">@elielsondev</a>
+      <a className="mt-1">@elielsondev</a>
     </main>
   );
 }
