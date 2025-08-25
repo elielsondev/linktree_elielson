92d5058a7c35be391f186fe076e39ed454f20622
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-08-25 02:00:33 -0300

    style - added the active when clicking the button

--- src/components/PropChildren.jsx
+++ src/components/PropChildren.jsx
@@ -1,6 +1,6 @@
 export default function PropChildren(props) {
   return (
-    <button className="bg-indigo-700 font-bold w-56 px-4 py-2 rounded hover:bg-slate-100 hover:text-green-600">
+    <button className="bg-indigo-700 font-bold w-56 px-4 py-2 rounded hover:bg-slate-100 hover:text-green-600 active:bg-green-600 active:text-slate-100">
       <a href={props.href} target="_blank">
         { props.children }
       </a>
